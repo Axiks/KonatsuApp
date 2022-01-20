dcbe3e7a996f162a96fc185c2ff76200fd6f66cf
Axiks <secure@mail.com>
2022-01-20 17:01:59 +0200

    Add user service

--- Konatsu.API/Services/UserService.cs
+++ Konatsu.API/Services/UserService.cs
@@ -9,39 +9,5 @@ using System.Threading.Tasks;
 
 namespace Konatsu.API.Services
 {
-    public class UserService : IUserService
-    {
-        private readonly DataContext _context;
-
-        public UserService(DataContext context)
-        {
-            _context = context;
-        }
-        public async void AddUser(string username)
-        {
-            var appUser = new AppUser
-            {
-                Username = username,
-            };
-
-            var result = await _context.AddAsync(appUser);
-            await _context.SaveChangesAsync();
-        }
-        public void DeleteUser(AppUser user)
-        {
-            var result = _context.Remove(user);
-            _context.SaveChanges();
-        }
-
-        public void Dispose()
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<AppUser> GetUser(int id)
-        {
-            var result = await _context.Users.Where(e => e.Id == id).FirstOrDefaultAsync();
-            return result;
-        }
-    }
+    
 }
